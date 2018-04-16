1b71c3aae1f5442f3fd5c36c0d3e05f66df3781a
HernanArena <hernan.arena@gmail.com>
2018-04-15 23:39:06 -0300

    Correcion de schema.sql

--- servidor/controladores/controlador.js
+++ servidor/controladores/controlador.js
@@ -5,9 +5,8 @@ function listarCompetencia(req, res) {
     var sql = "select * from competencia";
     con.query(sql, function(error, resultado, fields) {
       if (error) {
-          var string = "Hubo un error en la consulta "+error.message;
           console.log("Hubo un error en la consulta", error.message);
-          return res.status(404).send(string);
+          return res.status(404).send("Hubo un error en la consulta");
       }
       var response =  resultado;
       res.send(JSON.stringify(response));
